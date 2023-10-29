3d820fba65252dcdadee061398eb9fea46a9123b
Vandan Rohatgi <vandanrohatgi@gmail.com>
2023-10-29 18:52:57 +0530

    refactor: remove debugging, update dependencies

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -33,17 +33,10 @@ return {
         config = function()
             local mason = require("mason")
             local options = {
-                ensure_installed = {
-                    "delve",
-                }, -- not an option from mason.nvim
                 max_concurrent_installers = 10,
             }
 
             mason.setup(options)
-
-            vim.api.nvim_create_user_command("MasonInstallAll", function()
-                vim.cmd("MasonInstall " .. table.concat(options.ensure_installed, " "))
-            end, {})
         end
     },
     {
